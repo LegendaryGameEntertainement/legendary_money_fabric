bbe5231fe82a347891fa943465ac423c946ec979
MathieuDolphin <legendarygame.entertainement@gmail.com>
2024-10-04 12:31:05 +0200

    add first entities

---
+++ lua/entities/lg_paper/init.lua
@@ -0,0 +1,12 @@
+AddCSLuaFile("shared.lua");
+include("shared.lua");
+
+function ENT:Initialize()
+    self:SetModel("models/paper.mdl");
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)	
+	self:SetUseType(SIMPLE_USE)
+	local phys = self:GetPhysicsObject()
+	phys:Wake()
+end;
